a7f86e5199de487871f02143d0f5f22d296d6361
Suffering-Cephalopod <Neil-Kasson@users.noreply.github.com>
2025-02-11 02:36:10 -0500

    added Switches, Select, and some basic concurrency

--- control-flow/main.go
+++ control-flow/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 ////////////////////
@@ -14,7 +15,7 @@ func init(){
 
   //////////
  // Main // - neccessary and runs first to start program (unless init() is used)
-//////////
+//////////			-testing IF & SWITCH statements
 func main(){
 	fmt.Println("\nThis is the start of main().")
 	w := 42
@@ -25,8 +26,24 @@ func main(){
 	ifStatements(w, x)
 	ifStatements(x, y)
 	ifStatements(y, z)
-	fmt.Println("\nTesting if statement with 'idiom' function")
+	fmt.Println("\nTesting if statement with 'idiom' function:")
 	ifIdiom(x)
+	fmt.Println("\nSwitch statements with comparrison operators:")
+	switchA(x)
+	switchA(y)
+	switchA(z)
+	fmt.Println("\nDirect case switches:")
+	switchB(x)
+	switchB(y)
+	fmt.Println("\nSwitches with fallthrough:")
+	switchF(x)
+	switchF(y)
+
+	   /////////////////////
+	  // Move to main2() //
+	 //    ~line 140		//
+	/////////////////////
+	main2()
 }
 
 
@@ -52,9 +69,9 @@ func ifStatements(a, b int){
 ////////////
 func ifIdiom(a int){
 	if b:=2*rand.Intn(a); b>=a{			//  <----<--------	You can set a value in the if statement to lessen scope
-		fmt.Printf("Generated value %d is Greater than or Equal to %d", b, a)			//	This is called an 'idiom'.
+		fmt.Printf("Generated value %d is Greater than or Equal to %d.\n", b, a)			//	This is called an 'idiom'.
 	} else {
-		fmt.Printf("Generated value %d is Less than %d", b, a)
+		fmt.Printf("Generated value %d is Less than %d.\n", b, a)
 	}
 }
    ////////////////////////////////////////////////////////////////
@@ -68,3 +85,100 @@ func ifIdiom(a int){
 // 		return 0
 // 	}
 // }
+
+
+  ///////////////////////////
+ // Switch - conditionals //
+///////////////////////////
+func switchA(x int){
+	switch{
+	case x<783:
+		fmt.Printf("%d is less than 783.\n", x)
+	case x>783:
+		fmt.Printf("%d is greater than 783.\n", x)
+	case x==783:
+		fmt.Printf("%d equals 783.\n", x)
+	default:
+		fmt.Println("Switch test defaulted.")
+	}
+}
+
+  ///////////////////////////
+ // Switch - direct cases //
+///////////////////////////
+func switchB(x int){
+	switch x{
+	case 42:
+		fmt.Println("x is 42")
+	default:
+		fmt.Print("Default: x is not 42... ")
+		fmt.Printf("x is %d.\n", x)
+	}
+}
+
+  /////////////////////////////
+ // Switch with Fallthrough //
+/////////////////////////////
+func switchF(x int){
+	switch x{
+	case 42:
+		fmt.Println("x is 42")
+		fallthrough				//  <---<------- fallthrough means next case also executes.
+	case 37:
+		fmt.Println("x is still 42, not 37, but this is print anyway because of 'fallthrough'.")
+	case 837421:
+		fmt.Println("This won't print from fallthrough like the last case did.")
+	default:
+		fmt.Print("Default: x is not 42... ")
+		fmt.Printf("x is %d.\n", x)
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+func main2(){
+	fmt.Println("\n'Select' Statements and basic concurrency:")
+	selects()
+}
+
+func selects(){
+	// conncurrency
+	// select a channel
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	// get an int64, convert it to type 'time.Duration', then use it in 'time.Sleep()'.
+	// Int63n returns an int64
+	// type 'duration' has underlying type is int64
+	// time.Sleep() takes type 'duration'
+	// time.Millisecond is a constant in the 'time' package
+	d1 := time.Duration(rand.Int63n(250))
+	d2 := time.Duration(rand.Int63n(250))
+
+	// go routine - concurrency
+	go func(){
+		time.Sleep(d1 * time.Millisecond)
+		ch1 <- 41
+	}()
+	
+	go func(){
+		time.Sleep(d2 * time.Millisecond)
+		ch2 <- 42
+	}()
+
+	// A 'select' statement chooses which, from a set of possible send/recieve operations, will proceed.
+	// It looks similar to a 'switch' statement, but with all cases referring to communication operations
+	//		For more info: https://go.dev/ref/spec#Select_statements
+
+	// My notes on what's happening:
+	// Each channel 'sleeps' for a random mount of time.
+	// Which ever wakes up first, when that happens, the valus is sent to it's 'v', and that case is selected
+	select{
+	case v1 := <-ch1:
+		fmt.Println("Value from CH.1:", v1)
+	case v2 := <-ch2:
+		fmt.Println("Value from CH.2:", v2)
+	}
+}
